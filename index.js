73050115d8ea8d6d4d52d4b6004fa44e2b73dae9
Eduardo Castro <edusilva.dev@gmail.com>
2022-07-13 19:27:11 -0300

    feat: add route send mail

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ app.get('/', (req, res) => {
   return res.json({ server: 'Is up!' })
 })
 
+app.get('/send/mail', (req, res) => {
+  return res.status(200).json({ email: 'sent' })
+})
+
 app.listen(3000)
